c6e0494754c217b28d6a3a46aec32c9aedbc5dcd
James Growden <jgrowden@tenstorrent.com>
2026-07-27 08:13:27 -0400

    boards: tenstorrent: mk_bu_dmc: Update to u375
    Update to the U375; the U385 was chosen for pinctrl testing
    on the nucleo, but the actual board has a u375.
    
    Signed-off-by: James Growden <jgrowden@tenstorrent.com>

--- boards/tenstorrent/tt_grendel_mk_bu_dmc/board.cmake
+++ boards/tenstorrent/tt_grendel_mk_bu_dmc/board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-# Configure pyocd runner for STM32U385
-board_runner_args(pyocd "--target=stm32u385rgtxq")
+# Configure pyocd runner for STM32U375
+board_runner_args(pyocd "--target=stm32u375rgt6")
 
 # Include common pyocd board configuration
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
