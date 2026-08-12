7af62a622ad8435c89e6575b4f176286ccb13bcc
James Growden <jgrowden@tenstorrent.com>
2026-08-12 10:22:43 -0400

    github: ci: Target galaxy revc runner
    Target the galaxy revC runner, which should have the new label.
    
    Tests will not work on the old one, as tt-flash will correctly determine
    the old galaxy FW bundle is not acceptable for the revC, and therefore
    tests will run on whatever was flashed before.
    
    Remove the old board for now since we don't have a runner for it.
    
    Signed-off-by: James Growden <jgrowden@tenstorrent.com>

--- scripts/rev2board.sh
+++ scripts/rev2board.sh
@@ -26,6 +26,8 @@ case "$1" in
   fi;;
   p100a|p150a|p150b|p150c|p300a|p300b|p300c|galaxy|galaxy_revc|orion_slt)
   BOARD="tt_blackhole@$1/tt_blackhole/$CLUSTER";;
+  bh-galaxy-revc)
+  BOARD="tt_blackhole@galaxy_revc/tt_blackhole/$CLUSTER";;
   bh-galaxy)
   BOARD="tt_blackhole@galaxy/tt_blackhole/$CLUSTER";;
   loudbox)
